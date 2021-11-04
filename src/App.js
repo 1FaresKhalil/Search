b6469460005a39a0341e91a05b75beb17501ee44
1Fareskhalil <cds.faresmohamad15347@alexu.edu.eg>
2021-11-04 20:35:26 +0200

    commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
+
+const App = () => {
+  return (
+    <div>
+      <Search />
+    </div>
+  );
+};
+export default App;
